8c8826007f501a9047a3556431a2220f96bb7a18
Karan samrat <karansamrat05039@gmail.com>
2024-03-09 17:36:18 +0530

    models added

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "json-web-token";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -32,21 +34,58 @@ const userSchema = new Schema(
     },
     watchHistory: [
       {
-        type:Schema.Types.ObjectId,
-        ref:"Video"
-      }
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
     password: {
       type: String,
-      required: [true  , 'password is required'],
-     
+      required: [true, "password is required"],
     },
     refreshToken: {
       type: String,
-      
-    }
+    },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password"));
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+  // here we checked that if password is not modified  by !  sign dont do anything otherwise it will bcrypt will incrypt the password
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+  // here we use bcrypt to compare the pasword of user and incrypted by the bcrypt below two more function where we added our own methods and we can also make more methods as per neended
+};
+
+userSchema.methods.genrateAcessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.genrateRefreshToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
--- src/models/Video.model.js
+++ src/models/Video.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -18,9 +19,28 @@ const videoSchema = new Schema(
       type: String,
       required: true,
     },
+    duration: {
+      type: Number, // 
+      required: true,
+    },
+    views: {
+      type: Number, 
+      default:0
+    },
+    isPunlished: {
+      type: Boolean, // 
+      default:true
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
 
   { timestamps: true }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
 
 export const Video = mongoose.model("Video", videoSchema);
