2a1365dca3675c48793d7e72b6c9ce9433a96d15
Karan samrat <karansamrat05039@gmail.com>
2024-03-09 11:45:40 +0530

    added some app.use prop in app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
 
 const app  = express()
+app.use(cors(
+  {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+app.use(express.json({limit:'16kb'}))
+app.use(express.urlencoded({extended: true, limit:'16kb'}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 export {app}
+
+
+
+// by using this use multiple time we are configuring our backend like first we define cross origin by CORS then we set limit of json data that we get from server then we define the url path so this is called configuring our backend.
