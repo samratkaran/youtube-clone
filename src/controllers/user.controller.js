cbeeb820c938f65f7b488ccf6f85f8ec1fdf5f0a
Karan samrat <karansamrat05039@gmail.com>
2024-03-10 22:27:04 +0530

    user controller added and some bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,19 +1,83 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiErrror.js"
+import {User} from "../models/User.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUSer = asyncHandler(async (req ,res)=>{
   //get user details from frontend
-  // validation 
-  // check if user is already exsist
+  // validation (25-30)
+  // check if user is already exsist (38)
   // if not exists then create user
-  // check for images , check for avtaar
-  // upload them to cloudinary ,avtaar
-  // create usr object  - create entry in db
-  //remove password and refresh token field from response
-  // check for user creation
-  // return response 
+  // check for images , check for avtaar(41)
+  // upload them to cloudinary ,avtaar(50)
+  // create usr object  - create entry in db (58)
+  //remove password and refresh token field from response (68)
+  // check for user creation(72)
+  // return response  (78)
 
     const {username , email , fullname, password} =   req.body
     console.log("email:", email)
+    // if (fullname==="") {
+    //   throw new ApiError(400 , "full name is required")
+    // }
+    // this is common practice but in this we have to use multiple if else so we gonna use diffrent approch which is used in indrustrial usage
+
+    // new approach
+
+    if (
+      [fullname ,email , username,password].some((field)=>field?.trim() ==="")
+    ) {
+      throw new ApiError(400 , "all fields are required")
+    }
+
+    const existedUser = User.findOne({
+      $or:[{username},{email}]
+    })
+    //this code will check the username and email it is new so ask to chatGPT
+
+    if (existedUser) {
+      throw new ApiError(409 , "user alredy found ")
+    }
+
+    const avtarLocalPath = req.files?.avatar[0]?.path;
+
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avtarLocalPath) {
+      throw new ApiError(400 , "avtar file is required")
+    }
+
+    const avtar = await uploadOnCloudinary(avtarLocalPath)
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avtar) {
+      throw new ApiError(400 , "avtar file is required")
+    }
+
+    const user  = await User.create({
+      fullname,
+      avtar:avtar.url,
+      coverImage:coverImage?.url || "",
+      //we used this cause cover image is not required
+      email,
+      password,
+      username:username.toLoweCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+      throw new ApiError(500 , "somthing went wrong while registring the uer")
+    }
+
+
+return res.status(201).json(
+  new ApiResponse(200 , createdUser ,"user register successful")
+)
 
 
 
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password"));
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
   // here we checked that if password is not modified  by !  sign dont do anything otherwise it will bcrypt will incrypt the password
 });
