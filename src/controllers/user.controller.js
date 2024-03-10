8b9485ed60f93ee8720c8be557dd2f772927dedb
Karan samrat <karansamrat05039@gmail.com>
2024-03-10 21:13:05 +0530

    first apicheck on postman is done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUSer = asyncHandler(async (req ,res)=>{
   res.status(200).json({
-    message:"ok"
+    message:"karan is now on portal"
   })
 })
 
