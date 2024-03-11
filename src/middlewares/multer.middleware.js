05cc367220a54b7c52cf3b6d15157c8d334926b5
Karan samrat <karansamrat05039@gmail.com>
2024-03-12 00:13:41 +0530

    mlter error

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,37 +1,15 @@
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//   destination:function(req,file,cb){
-//     cb(null,"../../public/tempfile")
-//   },
-//   filename:function(req,file,cb){
-   
-//     cb(null , file.originalname )
-//   }
-// })
-
-// export const upload = multer({
-//   storage:storage
-// })
-// few chnages in folder
-
-
 import multer from "multer";
-import path from "path";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Define the destination directory
-    const destinationPath = path.join(__dirname, ".../../public/tempfile");
-    cb(null, destinationPath);
-  },
-  filename: function (req, file, cb) {
-    // Generate a unique filename for the uploaded file
-    cb(null, file.originalname);
-  }
-});
-
-export const upload = multer({
-  storage: storage,
-  // You can add additional configuration options here
-});
+    destination: function (req, file, cb) {
+      cb(null, "./public/tempfile")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
