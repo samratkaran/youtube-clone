494bd22fc6b08b69a7cc9c283040fdcb504eca26
Karan samrat <karansamrat05039@gmail.com>
2024-03-19 12:02:28 +0530

    router added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { logOutUser, loginUser, refreshAccessToken, registerUSer } from "../controllers/user.controller.js";
+import {
+   changeCurrentPassword,
+    getCurrentUser,
+     getUserChannelProfile,
+      getWatchHistory,
+       logOutUser,
+        loginUser, 
+        refreshAccessToken,
+         registerUSer,
+          updateAccountDetails,
+           updateUserAvatar,
+            updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,6 +37,16 @@ router.route("/register").post(
 
   router.route("/logout").post(verfiyJWT, logOutUser)
   router.route("/refreshtoken").post(refreshAccessToken)
+  router,router("/change-password").post(verfiyJWT , changeCurrentPassword)
+  router.route("/current-user").get(verfiyJWT , getCurrentUser)
+  router.route("/update-accout").patch(verfiyJWT  ,updateAccountDetails)
+  router.route("/update-avatar").patch(verfiyJWT , upload.single("avatar") , updateUserAvatar)
+  router.route("/update-cover-image").patch(verfiyJWT , upload.single("/coverImage") , updateUserCoverImage)
+
+  router.route("/c/:username").get(verfiyJWT , getUserChannelProfile)
+  router.route("/history").get(verfiyJWT , getWatchHistory)
+   
+
   
   // in this route we can see that how middleware is work we use verifyjwt as a middle warre just befrore using loogout and in verifyjwt we passed next() that will go to logoutuser after completeing its task if we want to use multiple middelware so we just use next() at them in the bottom ex:- royter.route("/logout").post(verfityjwt , verifyuser , logoutUser)
 
