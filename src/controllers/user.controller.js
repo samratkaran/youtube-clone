c07ec111914f7c9cccbb751d4f595ad381854ca3
Karan samrat <karansamrat05039@gmail.com>
2024-03-21 13:27:20 +0530

    changed in routes change password to patch

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -196,50 +196,99 @@ const logOutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, `user logged out successfully`));
 });
 
+// const refreshAccessToken = asyncHandler(async (req, res) => {
+
+//   const incomingRefreshToken =
+//     req.cookies.refreshToken || req.body.refreshToken;
+
+//   if (!incomingRefreshToken) {
+//     throw new ApiError(401, "unauthorized request");
+//   }
+//   try {
+//     const decodedToken = jwt.verify(
+//       incomingRefreshToken,
+//       process.env.REFRESH_TOKEN_SECRET
+//     );
+//     const user = await User.findById(decodedToken?._id);
+
+//     if (!user) {
+//       throw new ApiError(401, "invalid refresh token");
+//     }
+
+//     if (incomingRefreshToken !== user?.refreshToken) {
+//       throw new ApiError(401, "refresh token is expired or invalid");
+//     }
+
+//     const { accessToken, newRefreshToken } = await genrateAcessAndRefreshTokens(
+//       user._id
+//     );
+
+//     const options = {
+//       httpOnly: true,
+//       secure: true,
+//     };
+//     return res
+//       .status("accessToken", accessToken, options)
+//       .cookie("refreshToken", newRefreshToken, options)
+//       .json(
+//         new ApiResponse(
+//           200,
+//           { accessToken, refreshToken: newRefreshToken },
+//           "access token refreshed"
+//         )
+//       );
+//   } catch (error) {
+//     throw new ApiError(401, error?.message || "invalid refresh token");
+//   }
+// });
+
 const refreshAccessToken = asyncHandler(async (req, res) => {
-  const incomingRefreshToken =
-    req.cookies.refreshToken || req.body.refreshToken;
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
   if (!incomingRefreshToken) {
-    throw new ApiError(401, "unauthorized request");
+      throw new ApiError(401, "unauthorized request")
   }
+
   try {
-    const decodedToken = jwt.verify(
-      incomingRefreshToken,
-      process.env.REFRESH_TOKEN_SECRET
-    );
-    const user = await User.findById(decodedToken?._id);
-
-    if (!user) {
-      throw new ApiError(401, "invalid refresh token");
-    }
-
-    if (incomingRefreshToken !== user?.refreshToken) {
-      throw new ApiError(401, "refresh token is expired or invalid");
-    }
-
-    const { accessToken, newRefreshToken } = await genrateAcessAndRefreshTokens(
-      user._id
-    );
-
-    const options = {
-      httpOnly: true,
-      secure: true,
-    };
-    return res
-      .status("accessToken", accessToken, options)
+      const decodedToken = jwt.verify(
+          incomingRefreshToken,
+          process.env.REFRESH_TOKEN_SECRET
+      )
+  
+      const user = await User.findById(decodedToken?._id)
+  
+      if (!user) {
+          throw new ApiError(401, "Invalid refresh token")
+      }
+  
+      if (incomingRefreshToken !== user?.refreshToken) {
+          throw new ApiError(401, "Refresh token is expired or used")
+          
+      }
+  
+      const options = {
+          httpOnly: true,
+          secure: true
+      }
+  
+      const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
+  
+      return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
       .cookie("refreshToken", newRefreshToken, options)
       .json(
-        new ApiResponse(
-          200,
-          { accessToken, refreshToken: newRefreshToken },
-          "access token refreshed"
-        )
-      );
+          new ApiResponse(
+              200, 
+              {accessToken, refreshToken: newRefreshToken},
+              "Access token refreshed"
+          )
+      )
   } catch (error) {
-    throw new ApiError(401, error?.message || "invalid refresh token");
+      throw new ApiError(401, error?.message || "Invalid refresh token")
   }
-});
+
+})
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
   const { oldPassword, newPassword } = req.body;
@@ -397,7 +446,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         avatar: 1,
         coverImage: 1,
         email: 1,
-        watchHistory:1
+        watchHistory: 1,
       },
     },
   ]);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import {
-   changeCurrentPassword,
-    getCurrentUser,
-     getUserChannelProfile,
-      getWatchHistory,
-       logOutUser,
-        loginUser, 
-        refreshAccessToken,
-         registerUSer,
-          updateAccountDetails,
-           updateUserAvatar,
-            updateUserCoverImage } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  logOutUser,
+  loginUser,
+  refreshAccessToken,
+  registerUSer,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -19,35 +20,37 @@ const router = Router();
 router.route("/register").post(
   upload.fields([
     {
-      name:"avatar",
-      maxCount:1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-      name:"coverImage",
-      maxCount:1
-    }
+      name: "coverImage",
+      maxCount: 1,
+    },
     //here we used multer for upload file and avtaar just before registerUSer cause it is a middleware
   ]),
-  registerUSer)
+  registerUSer
+);
 
-  router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
+// secured routers
 
-  // secured routers
+router.route("/logout").post(verfiyJWT, logOutUser);
+router.route("/change-password").patch(verfiyJWT, changeCurrentPassword);
+router.route("/current-user").get(verfiyJWT, getCurrentUser);
+router.route("/update-accout").patch(verfiyJWT, updateAccountDetails);
+router.route("/refreshtoken").post(refreshAccessToken);
+router
+  .route("/update-avatar")
+  .patch(verfiyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verfiyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-  router.route("/logout").post(verfiyJWT, logOutUser)
-  // router.route("/refreshtoken").post(refreshAccessToken)
-  router.route("/change-password").post(verfiyJWT , changeCurrentPassword)
-  router.route("/current-user").get(verfiyJWT , getCurrentUser)
-  router.route("/update-accout").patch(verfiyJWT  ,updateAccountDetails)
-  // router.route("/update-avatar").patch(verfiyJWT , upload.single("avatar") , updateUserAvatar)
-  // router.route("/update-cover-image").patch(verfiyJWT , upload.single("coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(verfiyJWT, getUserChannelProfile);
+router.route("/history").get(verfiyJWT, getWatchHistory);
 
-  router.route("/c/:username").get(verfiyJWT , getUserChannelProfile)
-  router.route("/history").get(verfiyJWT , getWatchHistory)
-   
-
-  
-  // in this route we can see that how middleware is work we use verifyjwt as a middle warre just befrore using loogout and in verifyjwt we passed next() that will go to logoutuser after completeing its task if we want to use multiple middelware so we just use next() at them in the bottom ex:- royter.route("/logout").post(verfityjwt , verifyuser , logoutUser)
+// in this route we can see that how middleware is work we use verifyjwt as a middle warre just befrore using loogout and in verifyjwt we passed next() that will go to logoutuser after completeing its task if we want to use multiple middelware so we just use next() at them in the bottom ex:- royter.route("/logout").post(verfityjwt , verifyuser , logoutUser)
 
 export default router;
